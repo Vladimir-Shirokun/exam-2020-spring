e3021b025f3468f606f4475425804804fd48425e
packman <36705530+Vladimir-Shirokun@users.noreply.github.com>
2020-06-30 13:43:36 +0300

    To proof that it is working
    Не увидел коммита в первый раз, залью еще раз на всяякий пожарный.
    Port 8080

--- Shirokun/main.tf
+++ Shirokun/main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "instance" {
 
   ingress {
     from_port = "${var.server_port}"
-    to_port = "${var.server_port}"
+    to_port = "${var.server_port}" 
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
